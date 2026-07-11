e79349d3289c59ec22523948afc08af51473528f
Ariyanmahammad <ariyanmahammad5@gmail.com>
2026-07-11 22:17:06 +0530

    Python Day 1

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Hey , this is Ariyan!")
+#Hello i am Ariyan. Thisis single line comment.
+"""doctsring for multi line comments"""
+#variables
+Ariyan = 51
+
+#camel case 
+ariyanMahammad=51
+#pascal case 
+AriyanMahammad=51
+#snake case 
+Ariyan_Mahammad=51
+
+#data types 
+a=10 #Integer
+b=10.5 #float
+c=10/2 #float
+
+d=10j #complex
+#Strings - '' or ""
+Name = "Sk Ariyan Mahammad" # string data type
+
+Success = True # boolean data type
